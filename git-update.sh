1780ce02ca9daacb109b8aac01ae6274a1f28290
Martin Matejčík <matejcik.martin@gmail.com>
2020-01-24 13:35:04 +0100

    update git-update

--- git-update.sh
+++ git-update.sh
@@ -7,7 +7,26 @@ SOURCE_BRANCH=$2
 DESTINATION_REPO="$3"
 DESTINATION_BRANCH="$4"
 
-GIT_SSH_COMMAND="ssh -v"
+if ! echo $SOURCE_REPO | grep '.git'
+then
+  if [[ -n "$SSH_PRIVATE_KEY" ]]
+  then
+    SOURCE_REPO="git@github.com:${SOURCE_REPO}.git"
+    GIT_SSH_COMMAND="ssh -v"
+  else
+    SOURCE_REPO="https://github.com/${SOURCE_REPO}.git"
+  fi
+fi
+if ! echo $DESTINATION_REPO | grep '.git'
+then
+  if [[ -n "$SSH_PRIVATE_KEY" ]]
+  then
+    DESTINATION_REPO="git@github.com:${DESTINATION_REPO}.git"
+    GIT_SSH_COMMAND="ssh -v"
+  else
+    DESTINATION_REPO="https://github.com/${DESTINATION_REPO}.git"
+  fi
+fi
 
 echo "SOURCE=$SOURCE_REPO:$SOURCE_BRANCH"
 echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
